92f639ad9088dd458285bb82bd2c4b94fa65dd4d
Raphael Rebel <you@example.comraphaelrebel@live.com>
2021-01-13 15:44:57 +0100

    Landings page update

---
+++ Museum-code/Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "question.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is de locatie van Media College MBO",
+				"answers": [{
+						"answer": "Amsterdam",
+						"feedback": true
+					},
+					{
+						"answer": "Rotterdam",
+						"feedback": false
+					},
+					{
+						"answer": "Utrecht",
+						"feedback": false
+					},
+					{
+						"answer": "Haarlem",
+						"feedback": false
+					},
+					{
+						"answer": "Groningen",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welke nieuwe opleidingen zijn er dit jaar bij gekomen? Er zijn 2 goede antwoorden.",
+				"answers": [{
+						"answer": "Mediavormgeving",
+						"feedback": false
+					},
+					{
+						"answer": "Fotografie",
+						"feedback": true
+					},
+					{
+						"answer": "Audio visueel",
+						"feedback": false
+					},
+					{
+						"answer": "Software development",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Wat is jouw lievelings eten in de kantine",
+				"answers": [{
+						"answer": "frikandel broodje",
+						"feedback": true
+					},
+					{
+						"answer": "Chips",
+						"feedback": true
+					},
+					{
+						"answer": "Broodje gehakt",
+						"feedback": true
+					},
+					{
+						"answer": "Kippensoep",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
